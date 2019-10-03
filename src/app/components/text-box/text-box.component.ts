c8f40a5f9274849bf7bdc6922e858d6bc581d0af
Sammy <sami.shamailov@gmail.com>
2019-10-03 23:46:49 +0300

    initial commit

---
+++ src/app/components/text-box/text-box.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-box',
+  templateUrl: './text-box.component.html',
+  styleUrls: ['./text-box.component.css']
+})
+export class TextBoxComponent implements OnInit {
+  tweetContent: string;
+
+  constructor() { }
+
+  postTweet() {
+    // TODO call a service.
+    console.log(this.tweetContent);
+    this.tweetContent = '';
+  }
+
+  ngOnInit() {
+  }
+
+}
