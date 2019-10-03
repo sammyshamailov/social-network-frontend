c7b34529e3300d2e481f3698f4808090221a4fe1
Sammy <sami.shamailov@gmail.com>
2019-10-03 22:33:16 +0300

    initial commit

---
+++ src/app/components/tweets/tweets.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tweets',
+  templateUrl: './tweets.component.html',
+  styleUrls: ['./tweets.component.css']
+})
+export class TweetsComponent implements OnInit {
+
+  tweetArray = [
+    {
+      name: 'John Doe',
+      date: '03/10/20190',
+      content: 'thsfsdfsdfsdfsdfsfaofi aoknf oanoajdn asodn asodn oasdn osaidn osaind oasind oasnd asiodn asoidn asoidn'
+    },
+    {
+      name: 'John Doe',
+      date: '03/10/20190',
+      content: 'thsfsdfsdfsdfsdfsfaofi aoknf oanoajdn asodn asodn oasdn osaidn osaind oasind oasnd asiodn asoidn asoidn'
+    },
+    {
+      name: 'John Doe',
+      date: '03/10/20190',
+      content: 'thsfsdfsdfsdfsdfsfaofi aoknf oanoajdn asodn asodn oasdn osaidn osaind oasind oasnd asiodn asoidn asoidn'
+    }
+    ,
+    {
+      name: 'John Doe',
+      date: '03/10/20190',
+      content: 'thsfsdfsdfsdfsdfsfaofi aoknf oanoajdn asodn asodn oasdn osaidn osaind oasind oasnd asiodn asoidn asoidn'
+    }
+    ,
+    {
+      name: 'John Doe',
+      date: '03/10/20190',
+      content: 'thsfsdfsdfsdfsdfsfaofi aoknf oanoajdn asodn asodn oasdn osaidn osaind oasind oasnd asiodn asoidn asoidn'
+    }
+    ,
+    {
+      name: 'John Doe',
+      date: '03/10/20190',
+      content: 'thsfsdfsdfsdfsdfsfaofi aoknf oanoajdn asodn asodn oasdn osaidn osaind oasind oasnd asiodn asoidn asoidn'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
